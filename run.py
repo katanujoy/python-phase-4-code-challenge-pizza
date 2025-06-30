dda51696357f863469437b3951661744f779f935
katanujoy <katanujoyy99@gmail.com>
2025-07-01 00:30:40 +0300

    fixed up run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+# Import the Flask app factory function
 from server import create_app
 
+# Create an instance of the Flask app
 app = create_app()
 
+# Run the app only if this file is executed directly
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
